3cef1c7407ab38dc2966baec9831aced1fcaf5a1
Sebastian Pobocha <sebastianpob@gmail.com>
2021-05-31 01:41:43 +0200

    import assets from email and refactor index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,34 @@
 import React, { useState, useEffect } from 'react'
-
+import MapComponent from '@viewsComp/MapComponent'
 const { MYAPI_URL } = process.env
 
 const Home = () => {
-  const [desckId, desckIdSet] = useState('')
+  const [pickedOption, pickedOptionSet] = useState(0)
+  const avaliableFilterOpitions = [
+    { name: '0-250' },
+    { name: '250-500' },
+    { name: '500-1000' },
+    { name: '1000+' },
+  ]
 
-  return <div className="cardPage"></div>
+  useEffect(() => {
+    fetch(MYAPI_URL + '/state')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, [])
+  return (
+    <div id="indexPage">
+      <select
+        value={pickedOption}
+        onChange={e => pickedOptionSet(parseInt(e.target.value))}
+      >
+        {avaliableFilterOpitions.map((elem, i) => (
+          <option value={i}>{elem.name}</option>
+        ))}
+      </select>
+      <MapComponent />
+    </div>
+  )
 }
 
 export default Home
